f7766d84af2e0c8c08b1dbd5fa9693787a3ae5ca
Adrenaline9 <106984663+Adrenaline9@users.noreply.github.com>
2022-07-11 19:52:04 +0100

    Create ContactInfoItem.js

---
+++ src/components/ContactInfoItem.js
@@ -0,0 +1,129 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const FormStyle = styled.form`
+  width: 100%;
+  .form-group {
+    width: 100%;
+    margin-bottom: 2rem;
+  }
+  label {
+    font-size: 1.8rem;
+  }
+  input,
+  textarea {
+    width: 100%;
+    font-size: 2rem;
+    padding: 1.2rem;
+    color: var(--gray-1);
+    background-color: var(--deep-dark);
+    outline: none;
+    border: none;
+    border-radius: 8px;
+    margin-top: 1rem;
+  }
+  textarea {
+    min-height: 250px;
+    resize: vertical;
+  }
+  button[type='submit'] {
+    background-color: var(--gray-1);
+    color: var(--black);
+    font-size: 2rem;
+    display: inline-block;
+    outline: none;
+    border: none;
+    padding: 1rem 4rem;
+    border-radius: 8px;
+    cursor: pointer;
+  }
+`;
+
+export default function ContactForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  return (
+    <>
+      <FormStyle>
+        <div className="form-group">
+          <label htmlFor="name">
+            Your Name
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">
+            Your Email
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">
+            Your message
+            <textarea
+              type="text"
+              id="message"
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">Send</button>
+      </FormStyle>
+    </>
+  );
+}
+import React from 'react';
+import { MdPlace } from 'react-icons/md';
+import styled from 'styled-components';
+import PText from './PText';
+
+const ItemStyles = styled.div`
+  padding: 2rem;
+  background-color: var(--deep-dark);
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  border-radius: 8px;
+  margin-bottom: 2rem;
+  .icon {
+    color: var(--white);
+    background-color: var(--gray-2);
+    padding: 1.3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+  }
+  svg {
+    width: 3.5rem;
+  }
+`;
+
+export default function ContactInfoItem({
+  icon = <MdPlace />,
+  text = 'I need text ',
+}) {
+  return (
+    <ItemStyles>
+      <div className="icon">{icon}</div>
+      <div className="info">
+        <PText>{text}</PText>
+      </div>
+    </ItemStyles>
+  );
+}
