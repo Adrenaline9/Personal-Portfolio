be1d52ef9d0f8caecec8ce87f8a8cf3717e48d20
Adrenaline9 <bibiweipere@gmail.com>
2022-07-11 23:41:39 +0100

    1 file modified

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
+const image = "https://raw.githubusercontent.com/Adrenaline9/Personal-Portfolio/main/src/assets/images/img-2.jpeg";
 
 const projects = [
   {
@@ -6,35 +7,35 @@ const projects = [
     name: 'U Tracker',
     desc:
       'An application to track your all data from one place. I developed the website and the mobile app',
-    img: UTrackerImg,
+    img: {image},
   },
   {
     id: uuidv4(),
     name: 'Green CTG',
     desc:
       'An app to help people to get an overview of how they can make the city beautiful.',
-    img: GreenCtgImg,
+    img: {image},
   },
   {
     id: uuidv4(),
     name: 'Coin Tracker',
     desc:
       'Using this app you can track any e coin. Also you will get a good advise about investment form the professional',
-    img: CoinTrackerImg,
+    img: {image},
   },
   {
     id: uuidv4(),
     name: "Cavin's Portfolio",
     desc:
       'A portfolio for Cavin jr. A artist from New york city. The portfolio is made using ReactJs and GatsbyJs.',
-    img: CavinImg,
+    img: {image},
   },
   {
     id: uuidv4(),
     name: 'Tracking Soft',
     desc:
       'A tracking website that will show the performance of the website. Also you will get some useful advice to improve the performance.',
-    img: ProjectImg,
+    img: {image},
   },
 ];
 
