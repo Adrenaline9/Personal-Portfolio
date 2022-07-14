35274e2a4660e878706cd02905ad040b1c23915b
Adrenaline9 <106984663+Adrenaline9@users.noreply.github.com>
2022-07-14 06:25:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import NavMenu from './components/NavMenu';
 import ScrollToTop from './components/ScrollToTop';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
 import "./style.css";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-        <NavMenu />
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contacs" element={<Contact />} />
-          <Route path="/projects" element={<Projects />} />
-          </Route>
-        </Routes>
+const App = () => (
+  <div>
+   <NavMenu />
+   <ScrollToTop />
+   <Footer />
+  </div>
+  )
 
-        <Footer />
-          </BrowserRouter>
+export default App
 
-  );
-}
