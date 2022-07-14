f034dd1d83090592a0f5aded5b838dc829c03663
Adrenaline9 <bibiweipere@gmail.com>
2022-07-14 07:10:34 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from './components/Footer';
 import NavMenu from './components/NavMenu';
 import ScrollToTop from './components/ScrollToTop';
+import Main from './Main';
 import "./style.css";
 
 const App = () => (
