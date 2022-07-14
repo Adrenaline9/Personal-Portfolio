7f476dbd4b9977af35b1c8771b1e4663ce5a34a0
Adrenaline9 <106984663+Adrenaline9@users.noreply.github.com>
2022-07-14 06:36:50 +0100

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,22 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import "./style.css";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contacs" element={<Contact />} />
-          <Route path="/projects" element={<Projects />} />
-          </Route>
-        </Routes>
-     </BrowserRouter>
+const Main = () => (
+    <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
+
+       </Switch>
+    </main>
+
 
   );
 }
