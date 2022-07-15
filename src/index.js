129c3c7b1d173bb729bd92c985ab4fc38e05876b
Adrenaline9 <106984663+Adrenaline9@users.noreply.github.com>
2022-07-15 02:59:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Layout from './pages/Layout';
+import Projects from './pages/Projects';
+import NoPage from "./pages/NoPage";
+
+import "./style.css";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+          </Route>       
+        </Routes>
+    </BrowserRouter>
+ );
+}
+
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
