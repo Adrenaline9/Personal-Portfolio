0862386dc6f8412c61b380cf23d9e701dd63df9a
Adrenaline9 <bibiweipere@gmail.com>
2022-07-21 12:06:41 +0100

    1 file modified

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -7,10 +7,8 @@ import SectionTitle from './SectionTitle';
 import '../style.css';
 
 const AboutSectionStyles = styled.div`
-  margin: auto;
-  width: 100%;
   padding: 10rem 0;
-  .container {
+  .containers {
     display: flex;
     align-items: center;
     justify-content: flex-start;
@@ -20,10 +18,7 @@ const AboutSectionStyles = styled.div`
   .aboutSection__right {
     flex: 1;
   }
-  .section-title {
-    text-align: left;
-  }
-  }
+  
   .para {        
     margin-top: 2rem;
     margin-left: 0;
@@ -47,7 +42,7 @@ const AboutSectionStyles = styled.div`
     }
   }
   @media only screen and (max-width: 768px) {
-    .container {
+    .containers {
       flex-direction: column;
       text-align: center;
     }
@@ -81,23 +76,22 @@ export default function AboutSection() {
   return (
     <AboutSectionStyles>
       <div className="container">
-        <div className="aboutSection__left">
-            <SectionTitle
-              subheading="Let me introduce myself"
-              heading="About Me"
-            />
-          <PText>
-            I am a freelance website designer and developer. I create
-            professional websites and always try to show unique views to the
-            audience through my design...
-          </PText>
-          <div className="aboutSection__buttons">
-            <Button btnText="Read More" btnLink="/about" outline />
+        <SectionTitle subheading="Let me introduce myself" heading="About Me" />
+        <div className="containers">
+          <div className="aboutSection__left">
+            <PText>
+              I am a freelance website designer and developer. I create
+              professional websites and always try to show unique views to the
+              audience through my design...
+            </PText>
+            <div className="aboutSection__buttons">
+              <Button btnText="Read More" btnLink="/about" outline />
+            </div>
           </div>
-        </div>
-        <div className="aboutSection__right">
-          <div className="aboutimg_text">
-            <Adrenaline />
+          <div className="aboutSection__right">
+            <div className="aboutimg_text">
+              <Adrenaline />
+            </div>
           </div>
         </div>
       </div>
